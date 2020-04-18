cf279ba493418dc9fc9c89ef547acb62ace537af
Francesco Paolocci <micky5991@gmail.com>
2020-04-18 21:01:20 +0200

    Add documentation to IBankAccountFactory

--- Banking/Interfaces/IBankAccountFactory.cs
+++ Banking/Interfaces/IBankAccountFactory.cs
@@ -1,10 +1,21 @@
+using System;
 using JetBrains.Annotations;
 
 namespace Micky5991.Banking.Interfaces
 {
+    /// <summary>
+    /// Factory that is responsible to create <see cref="IBankAccount"/> instances.
+    /// </summary>
     [PublicAPI]
     public interface IBankAccountFactory
     {
-        IBankAccount CreateBankAccount(decimal balance, string? identifier = null);
+        /// <summary>
+        /// Method that creates a new <see cref="IBankAccount"/> instance with the given parameters.
+        /// </summary>
+        /// <param name="balance">Current balance this account has.</param>
+        /// <param name="identifier">Human readable identifier of this <see cref="IBankAccount"/>.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="identifier"/> is null, empty or whitespace.</exception>
+        /// <returns>Newly created instance of <see cref="IBankAccount"/>.</returns>
+        IBankAccount CreateBankAccount(decimal balance, string identifier);
     }
 }
