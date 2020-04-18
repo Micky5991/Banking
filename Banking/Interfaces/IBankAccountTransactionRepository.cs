fcae9a38c3241e638b19f286e45eed7ed84cab55
Francesco Paolocci <micky5991@gmail.com>
2020-04-18 21:15:45 +0200

    Document bankaccount transaction repository to persist logs

--- Banking/Interfaces/IBankAccountTransactionRepository.cs
+++ Banking/Interfaces/IBankAccountTransactionRepository.cs
@@ -1,10 +1,25 @@
-using System.Collections.Generic;
+using System;
+using JetBrains.Annotations;
 using Micky5991.Banking.Data;
 
 namespace Micky5991.Banking.Interfaces
 {
+    /// <summary>
+    /// Repository that abstracts common actions on a database in the context of <see cref="BankAccountTransaction"/>.
+    /// </summary>
+    [PublicAPI]
     public interface IBankAccountTransactionRepository
     {
-        ICollection<BankAccountTransaction> GetTransactions(IBankAccount bankAccount);
+        /// <summary>
+        /// Logs a transaction that should be assigned to <see cref="transactionReceipient"/> and remembers the <paramref name="transactionOpposite"/> <see cref="IBankAccount"/>.
+        /// Because the transactions are persisted in a double-entry ledger the <see cref="transactionReceipient"/> can be either the sender of this amount and the receiver.
+        /// </summary>
+        /// <param name="transactionReceipient">Reference to the <see cref="IBankAccount"/> that should this transaction be assigned to.</param>
+        /// <param name="transactionOpposite">Nullable reference to the opposite side of this transaction. If null, the opposite site cannot be determined.</param>
+        /// <param name="amount">Non-zero amount this transaction changed the balance of <paramref name="transactionReceipient"/> and <paramref name="transactionOpposite"/>.</param>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="amount"/> is zero.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="transactionReceipient"/> is null. <paramref name="reason"/> is null, empty or whitespace.</exception>
+        /// <param name="reason">Message that explained the reason of this transaction of the view of <paramref name="transactionReceipient"/>.</param>
+        void CreateTransaction(IBankAccount transactionReceipient, IBankAccount? transactionOpposite, decimal amount, string reason);
     }
 }
