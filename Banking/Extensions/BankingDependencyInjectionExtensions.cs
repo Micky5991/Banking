7817b955a6ebaaaf1e3c58b9013fef347a7d6b31
Francesco Paolocci <micky5991@gmail.com>
2020-04-19 09:47:56 +0200

    Add general documentation for BankingDependencyInjectionExtensions

--- Banking/Extensions/BankingDependencyInjectionExtensions.cs
+++ Banking/Extensions/BankingDependencyInjectionExtensions.cs
@@ -1,7 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Micky5991.Banking.Extensions
 {
+    /// <summary>
+    /// Provides extensions for <see cref="IServiceCollection"/> to simplify registrations.
+    /// </summary>
     public static class BankingDependencyInjectionExtensions
     {
-
     }
 }
