1ffd570ddd123b78be78ee9b65f92af37326a919
Francesco Paolocci <micky5991@gmail.com>
2020-04-19 09:45:10 +0200

    Streamline naming of parameters in BankAccountTransaction

--- Banking/Data/BankAccountTransaction.cs
+++ Banking/Data/BankAccountTransaction.cs
@@ -13,16 +13,16 @@ namespace Micky5991.Banking.Data
         /// <summary>
         /// Initializes a new instance of the <see cref="BankAccountTransaction"/> class.
         /// </summary>
-        /// <param name="transactionAccount"><see cref="IBankAccount"/> that was associated with this transaction.</param>
-        /// <param name="transactionOppositeAccount">Opposite <see cref="IBankAccount"/> of this transaction that receives/sent the <paramref name="amount"/>.</param>
+        /// <param name="transactionRecipient"><see cref="IBankAccount"/> that was associated with this transaction.</param>
+        /// <param name="transactionOpposite">Opposite <see cref="IBankAccount"/> of this transaction that receives/sent the <paramref name="amount"/>.</param>
         /// <param name="amount">Non-Zero amount which was added to the <see cref="IBankAccount"/>. This can be negative.</param>
         /// <param name="reason">Message that describes why this <see cref="BankAccountTransaction"/> has been executed.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="transactionAccount"/> was null. <paramref name="reason"/> was null, empty or whitespace.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="transactionRecipient"/> was null. <paramref name="reason"/> was null, empty or whitespace.</exception>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="amount"/> was 0.</exception>
-        public BankAccountTransaction(IBankAccount transactionAccount, IBankAccount? transactionOppositeAccount, decimal amount, string reason)
+        public BankAccountTransaction(IBankAccount transactionRecipient, IBankAccount? transactionOpposite, decimal amount, string reason)
         {
-            this.TransactionAccount = transactionAccount;
-            this.TransactionOppositeAccount = transactionOppositeAccount;
+            this.TransactionRecipient = transactionRecipient;
+            this.TransactionOpposite = transactionOpposite;
             this.Amount = amount;
             this.Reason = reason;
         }
@@ -30,15 +30,15 @@ namespace Micky5991.Banking.Data
         /// <summary>
         /// Gets the <see cref="IBankAccount"/> that owns this transaction.
         /// </summary>
-        public IBankAccount TransactionAccount { get; }
+        public IBankAccount TransactionRecipient { get; }
 
         /// <summary>
         /// Gets the opposite <see cref="IBankAccount"/> that either sent or received <see cref="Amount"/>.
         /// </summary>
-        public IBankAccount? TransactionOppositeAccount { get; }
+        public IBankAccount? TransactionOpposite { get; }
 
         /// <summary>
-        /// Gets the amount that was sent to or received from <see cref="TransactionOppositeAccount"/>.
+        /// Gets the amount that was sent to or received from <see cref="TransactionOpposite"/>.
         /// </summary>
         public decimal Amount { get; }
 
