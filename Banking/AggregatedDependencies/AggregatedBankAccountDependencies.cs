26523874b8f09deb75cb1e46dfa391a4768e2cb3
Francesco Paolocci <micky5991@gmail.com>
2020-04-19 09:40:18 +0200

    Add documentation for AggregatedBankAccountDependencies

--- Banking/AggregatedDependencies/AggregatedBankAccountDependencies.cs
+++ Banking/AggregatedDependencies/AggregatedBankAccountDependencies.cs
@@ -1,14 +1,25 @@
+using Micky5991.Banking.Entities;
 using Micky5991.Banking.Interfaces;
 
 namespace Micky5991.Banking.AggregatedDependencies
 {
+    /// <summary>
+    /// Container for all <see cref="BankAccount"/> dependencies.
+    /// </summary>
     public class AggregatedBankAccountDependencies
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregatedBankAccountDependencies"/> class.
+        /// </summary>
+        /// <param name="transactionRepository">Instance of <see cref="IBankAccountTransactionRepository"/>.</param>
         public AggregatedBankAccountDependencies(IBankAccountTransactionRepository transactionRepository)
         {
             this.TransactionRepository = transactionRepository;
         }
 
+        /// <summary>
+        /// Gets the dependency instance of <see cref="IBankAccountTransactionRepository"/>.
+        /// </summary>
         public IBankAccountTransactionRepository TransactionRepository { get; }
     }
 }
