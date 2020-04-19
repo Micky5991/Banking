613c82f15aa081768dc083a695edeacd32cd3742
Francesco Paolocci <micky5991@gmail.com>
2020-04-19 09:31:34 +0200

    Add documentation for BankAccountTransaction

--- Banking/Data/BankAccountTransaction.cs
+++ Banking/Data/BankAccountTransaction.cs
@@ -1,26 +1,50 @@
+using System;
+using JetBrains.Annotations;
 using Micky5991.Banking.Interfaces;
 
 namespace Micky5991.Banking.Data
 {
+    /// <summary>
+    /// Representation of a transaction of <see cref="IBankAccount"/>.
+    /// </summary>
+    [PublicAPI]
     public class BankAccountTransaction
     {
-        public BankAccountTransaction(object id, decimal amount, IBankAccount? sourceAccount, IBankAccount targetAccount, string reason)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BankAccountTransaction"/> class.
+        /// </summary>
+        /// <param name="transactionAccount"><see cref="IBankAccount"/> that was associated with this transaction.</param>
+        /// <param name="transactionOppositeAccount">Opposite <see cref="IBankAccount"/> of this transaction that receives/sent the <paramref name="amount"/>.</param>
+        /// <param name="amount">Non-Zero amount which was added to the <see cref="IBankAccount"/>. This can be negative.</param>
+        /// <param name="reason">Message that describes why this <see cref="BankAccountTransaction"/> has been executed.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="transactionAccount"/> was null. <paramref name="reason"/> was null, empty or whitespace.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="amount"/> was 0.</exception>
+        public BankAccountTransaction(IBankAccount transactionAccount, IBankAccount? transactionOppositeAccount, decimal amount, string reason)
         {
-            this.Id = id;
+            this.TransactionAccount = transactionAccount;
+            this.TransactionOppositeAccount = transactionOppositeAccount;
             this.Amount = amount;
-            this.SourceAccount = sourceAccount;
-            this.TargetAccount = targetAccount;
             this.Reason = reason;
         }
 
-        public object Id { get; }
+        /// <summary>
+        /// Gets the <see cref="IBankAccount"/> that owns this transaction.
+        /// </summary>
+        public IBankAccount TransactionAccount { get; }
 
+        /// <summary>
+        /// Gets the opposite <see cref="IBankAccount"/> that either sent or received <see cref="Amount"/>.
+        /// </summary>
+        public IBankAccount? TransactionOppositeAccount { get; }
+
+        /// <summary>
+        /// Gets the amount that was sent to or received from <see cref="TransactionOppositeAccount"/>.
+        /// </summary>
         public decimal Amount { get; }
 
-        public IBankAccount? SourceAccount { get; }
-
-        public IBankAccount TargetAccount { get; }
-
+        /// <summary>
+        /// Gets the message why this transaction was executed.
+        /// </summary>
         public string Reason { get; }
     }
 }
