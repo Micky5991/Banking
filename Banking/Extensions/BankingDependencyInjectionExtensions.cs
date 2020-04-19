09c37cc4a8acf11d815b2790e084a42829328685
Francesco Paolocci <micky5991@gmail.com>
2020-04-19 10:00:36 +0200

    Add methods that implement default service registration methods

--- Banking/Extensions/BankingDependencyInjectionExtensions.cs
+++ Banking/Extensions/BankingDependencyInjectionExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using Micky5991.Banking.AggregatedDependencies;
+using Micky5991.Banking.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Micky5991.Banking.Extensions
@@ -7,5 +10,44 @@ namespace Micky5991.Banking.Extensions
     /// </summary>
     public static class BankingDependencyInjectionExtensions
     {
+        /// <summary>
+        /// Registers all default banking services.
+        /// </summary>
+        /// <param name="serviceCollection">Collection of services in which these services should be registered to.</param>
+        /// <returns>Passed service in <paramref name="serviceCollection"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="serviceCollection"/> is null.</exception>
+        public static IServiceCollection AddDefaultBankingDependencies(this IServiceCollection serviceCollection)
+        {
+            serviceCollection
+                .AddDefaultAggregatedBankingDependencies()
+                .AddDefaultBankAccountFactory();
+
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Registes the default aggregated banking service dependencies.
+        /// </summary>
+        /// <param name="serviceCollection">Collection of services in which these services should be registered to.</param>
+        /// <returns>Passed service in <paramref name="serviceCollection"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="serviceCollection"/> is null.</exception>
+        public static IServiceCollection AddDefaultAggregatedBankingDependencies(
+            this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<AggregatedBankAccountDependencies>();
+
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Registers the default implementation for <see cref="IBankAccountFactory"/>.
+        /// </summary>
+        /// <param name="serviceCollection">Collection of services in which these services should be registered to.</param>
+        /// <returns>Passed service in <paramref name="serviceCollection"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="serviceCollection"/> is null.</exception>
+        public static IServiceCollection AddDefaultBankAccountFactory(this IServiceCollection serviceCollection)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
